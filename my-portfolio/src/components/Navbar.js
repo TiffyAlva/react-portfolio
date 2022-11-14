b5fd15dddf0569ab35fb246e26448c6d33263ecd
TiffyAlva <TiffanyAlvarez54@gmail.com>
2022-11-14 01:25:53 -0500

    created footer file and footer css

--- my-portfolio/src/components/Navbar.js
+++ my-portfolio/src/components/Navbar.js
@@ -1,4 +1,5 @@
-import "./NavbarStyles.css"
+import "../components/Navbar.js";
+import "../components/NavbarStyles.css";
 
 import React, {useState} from 'react'
 import { Link } from "react-router-dom"
@@ -10,8 +11,19 @@ const Navbar = () => {
     const [click, setClick] = useState(false);
     const handleClick = () => setClick(!click);
 
+    const [color, setColor] = useState(false);
+    const changeColor = () =>{
+        if(window.scrollY >= 100) {
+            setColor(true);
+        }else{
+            setColor(false);
+        }
+    };
+
+    window.addEventListener("scroll", changeColor);
+
   return (
-    <div className= "header">
+    <div className={color ? "header header-bg" : "header"}>
       <Link to="/">
         <h1>Portfolio</h1>
         </Link>  
