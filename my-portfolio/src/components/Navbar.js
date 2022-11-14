86ef62be4b535026524346b88435784da74cbae2
TiffyAlva <TiffanyAlvarez54@gmail.com>
2022-11-13 23:15:01 -0500

    created hero image folder and css

--- my-portfolio/src/components/Navbar.js
+++ my-portfolio/src/components/Navbar.js
@@ -1,15 +1,21 @@
 import "./NavbarStyles.css"
 
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from "react-router-dom"
 
+import { FaBars, FaTimes } from "react-icons/fa";
+
+
 const Navbar = () => {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
   return (
     <div className= "header">
       <Link to="/">
         <h1>Portfolio</h1>
         </Link>  
-        <ul className="nav-menu">
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li>
                 <Link to="/">Home</Link>
             </li>
@@ -26,8 +32,20 @@ const Navbar = () => {
                 <Link to="/resume">Resume</Link>
             </li>
         </ul>
-    </div>
-  )
-}
+        <div className="hamburger" onClick= {handleClick}>
+            {click ? (
+              <FaTimes size={20} style={{color: "#fff"}} />  
+            ) : (
+              <FaBars size={20} style={{color: "#fff"}} /> 
 
-export default Navbar
+            )}
+        </div>
+    </div>
+        );
+        
+    };
+            
+    export default Navbar
+  
+  
+
