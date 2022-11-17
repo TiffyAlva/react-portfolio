8654bb484b64a02f87f6d054dc5d4ec71c0fc6ef
TiffyAlva <TiffanyAlvarez54@gmail.com>
2022-11-16 20:38:04 -0500

    added resume

--- src/components/ResumeDoc.js
+++ src/components/ResumeDoc.js
@@ -3,17 +3,12 @@ import './ResumeDoc.css';
 
 
 function ResumeDoc(){
-    const  resumePdf = 'https://drive.google.com/file/d/1L41eMg_WeEUTZxfwKarE6l-XzBPQwWNJ/view?usp=share_link';
+    const  resumePdf = 'TiffanyResume.pdf';
 
     return (<>
-        <a className="btn download-btn fs-5 mb-3" href={`${ResumeDoc}`} download>Download</a>
+        <a className="btn download-btn fs-5 mb-3" href={`${resumePdf}`} download>Download</a>
 
-        <embed
-            className='mb-2'
-            src={`https://drive.google.com/file/d/1L41eMg_WeEUTZxfwKarE6l-XzBPQwWNJ/view?usp=share_link${ResumeDoc}/preview`}
-            style={{display: 'block', margin: '0 auto', width: '90%', minWidth: '300px', height: '70vw', minHeight: '600px'}}
-            width="75%"
-        />
+      
     </>);
 }
 
